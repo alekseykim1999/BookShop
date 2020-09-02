68f501e982ed40a60778b83a4f0ba31ce23442fb
aleksey <alex.ki99@mail.ru>
2020-09-02 18:58:55 +0300

    add-models

---
+++ Library/Controllers/InfoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    public class InfoController : Controller
+    {
+
+        public ViewResult AddAuthor() 
+        {
+
+           
+
+
+            return View(); 
+
+
+        }
+
+        public ViewResult AddPublisher() 
+        {
+            
+            return View();
+
+
+        }
+
+        public ViewResult AddBook()
+        {
+            
+            return View();
+
+        }
+
+    }
+}
