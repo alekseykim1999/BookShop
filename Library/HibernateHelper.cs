559e4ddd35eba611011b0822f6a94f5e8da98c8a
aleksey <alex.ki99@mail.ru>
2020-09-03 11:18:55 +0300

    add-oledb

---
+++ Library/HibernateHelper.cs
@@ -0,0 +1,40 @@
+﻿using Library.Data.Models;
+using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class HibernateHelper
+    {
+
+        private static ISessionFactory _sessionFactory;
+
+        private static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if(_sessionFactory==null)
+                {
+                    var cfg = new Configuration();
+                    cfg.Configure();
+                    cfg.AddAssembly(typeof(Author).Assembly);
+
+                    _sessionFactory = cfg.BuildSessionFactory();
+                }
+
+                return _sessionFactory;
+            }
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+     
+       
+    }
+}
--- Library/WorkClasses/Saviour.cs
+++ Library/WorkClasses/Saviour.cs
@@ -1,11 +1,14 @@
 ﻿using Library.Data.Models;
 using NHibernate;
+using NHibernate.Tool.hbm2ddl;
 using NHibernate.Cfg;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
 namespace Library.WorkClasses
 {
     //класс для сохранения в БД
@@ -14,38 +17,63 @@ namespace Library.WorkClasses
     public class Saviour
     {
 
-        private Configuration myConfiguration;
-        private ISessionFactory mySessionFactory;
-        private ISession mySession;
+        public static string connectString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=BookShop.mdb;";
+
+        OleDbConnection myConnection=new OleDbConnection(connectString);
+
 
         public Saviour()
         {
-            myConfiguration = new Configuration();
-            myConfiguration.Configure();
-
-            mySessionFactory = myConfiguration.BuildSessionFactory();
-            mySession = mySessionFactory.OpenSession();
+            
         }
         public void SaveAuthorToAccess(Author _author)
         {
+            
+            myConnection.Open();
 
-            using (mySession.BeginTransaction())
-            {
-                mySession.Save(_author);
-                mySession.Transaction.Commit();
-            }
-                
+
+            OleDbCommand worker = new OleDbCommand();
+            worker.Connection = myConnection;
+            worker.CommandType = CommandType.Text;
+            worker.CommandText = ("INSERT INTO [Authors]([AuthorName]) VALUES (@name)");
+            worker.Parameters.AddWithValue("@name", _author.AuthorName);
+            worker.ExecuteNonQuery();
+
+            myConnection.Close();
+            
+            //using (ISession session = HibernateHelper.OpenSession())
+            //{
+            //    using (ITransaction transaction = session.BeginTransaction())
+            //    {
+            //        session.Save(_author);
+            //        transaction.Commit();
+            //    }
+            //}
+
         }
 
         public void SavePublisherToAccess(Publisher _publisher)
         {
+            myConnection.Open();
 
-            using (mySession.BeginTransaction())
-            {
-                mySession.Save(_publisher);
-                mySession.Transaction.Commit();
-            }
 
+            OleDbCommand worker = new OleDbCommand();
+            worker.Connection = myConnection;
+            worker.CommandType = CommandType.Text;
+            worker.CommandText = ("INSERT INTO [Publishers]([PublisherName]) VALUES (@name)");
+            worker.Parameters.AddWithValue("@name", _publisher.PublisherName);
+            worker.ExecuteNonQuery();
+
+            myConnection.Close();
+
+            //using (ISession session = HibernateHelper.OpenSession())
+            //{
+            //    using (ITransaction transaction = session.BeginTransaction())
+            //    {
+            //        session.Save(_author);
+            //        transaction.Commit();
+            //    }
+            //}
         }
 
     }
